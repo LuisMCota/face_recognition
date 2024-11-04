90c6f5da3bf5509f35e7d18d913fc97a0f2ab74d
LuisMCota <luis.f.m.c@outlook.com>
2024-11-04 17:33:21 -0600

    log in

--- src/app.py
+++ src/app.py
@@ -9,18 +9,7 @@ from firebase_admin import credentials, auth, initialize_app
 import os
 
 # Crear el objeto de credenciales a partir de las variables de entorno
-cred = credentials.Certificate({
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
-})
+cred = credentials.Certificate('credentials.json')
 
 # Inicializar la app de Firebase si no está ya inicializada
 if not firebase_admin._apps:
