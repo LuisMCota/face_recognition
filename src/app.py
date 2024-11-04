fb5bb9825ddd5426f4aaa5e2a72e02eb55e5374e
LuisMCota <luis.f.m.c@outlook.com>
2024-11-04 17:45:12 -0600

    log in

--- src/app.py
+++ src/app.py
@@ -1,72 +1,35 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from api_utils import fetch_attendance_data, send_image_for_prediction, register_student
+from src.api_utils import fetch_attendance_data, send_image_for_prediction, register_student
 from attendance_utils import apply_filters, display_totals, display_attendance_chart
 from style import set_styles
 import pandas as pd
 import firebase_admin
-from firebase_admin import credentials, auth, initialize_app
+from firebase_admin import credentials, initialize_app
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 # Crear el objeto de credenciales a partir de las variables de entorno
-cred = credentials.Certificate('credentials.json')
+cred = credentials.Certificate({
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+})
 
 # Inicializar la app de Firebase si no está ya inicializada
 if not firebase_admin._apps:
     initialize_app(cred)
+    
 set_styles()
-
-# Inicializar el estado de sesión si no existe
-if "authenticated" not in st.session_state:
-    st.session_state["authenticated"] = False
-    st.session_state["email"] = ""
-    st.session_state["login_success"] = False  # Flag para manejar la redirección
-
-# Pantalla de autenticación estilizada
-def login_screen():
-    st.title("🌐 Bienvenido a la Plataforma de Asistencia")
-    st.write("Selecciona una opción para continuar:")
-
-    # Opciones de inicio de sesión y registro con clave única
-    option = st.radio("", ["Iniciar Sesión", "Registrarse"], index=0, key="auth_option")
-
-    # Entrada para el correo electrónico y la contraseña
-    email = st.text_input("📧 Correo electrónico")
-    password = st.text_input("🔑 Contraseña", type="password")
-
-    # Registro
-    if option == "Registrarse" and st.button("Crear cuenta"):
-        if email and password:
-            try:
-                auth.create_user(email=email, password=password)
-                st.success("✨ ¡Registro exitoso! Inicia sesión para continuar.")
-            except Exception as e:
-                st.error(f"Error en el registro: {e}")
-        else:
-            st.warning("🔔 Completa ambos campos para registrarte.")
-
-    elif option == "Iniciar Sesión" and st.button("Iniciar Sesión"):
-        if email.strip() and password.strip():
-            try:
-                user = auth.get_user_by_email(email)
-                st.session_state["user"] = user.uid
-                st.session_state["email"] = email
-                st.session_state["authenticated"] = True
-                st.session_state["login_success"] = True  # Marca el inicio de sesión exitoso
-                st.success("🔓 Inicio de sesión exitoso. Redirigiendo a la plataforma...")
-            except Exception as e:
-                st.error("⚠️ Usuario o contraseña incorrectos.")
-        else:
-            st.warning("🔔 Completa ambos campos para iniciar sesión.")
-
-# Función para cerrar sesión
-def logout():
-    if st.button("Cerrar Sesión"):
-        st.session_state["authenticated"] = False
-        st.session_state["login_success"] = False  # Resetear el flag de éxito de login
-        st.session_state.pop("user", None)
-        st.session_state.pop("email", None)
-        st.success("Sesión cerrada exitosamente.")
 
 # Panel de asistencia
 def mostrar_asistencia():
@@ -107,43 +70,31 @@ def agregar_alumno():
         else:
             st.error(mensaje)
 
-# Lógica principal de autenticación y enrutamiento
-if not st.session_state["authenticated"]:
-    if st.session_state["login_success"]:
-        # Si el inicio de sesión fue exitoso, eliminamos el flag y recargamos para mostrar el menú
-        st.session_state["login_success"] = False
-        st.experimental_rerun()
-    else:
-        login_screen()
-else:
-    # Menú de navegación y lógica de enrutamiento
-    with st.sidebar:
-        st.write(f"👤 Usuario: {st.session_state['email']}")
-        logout()
+# Menú de navegación y lógica de enrutamiento
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Menú Principal",
+        options=["Panel General", "Agregar Alumno", "Tomar Asistencia"],
+        icons=["house", "person-plus", "camera"],
+        menu_icon="cast",
+        default_index=0,
+        styles={
+            "container": {"padding": "5px", "background-color": "#B0B0B0"},
+            "icon": {"color": "#FFA500", "font-size": "20px"},
+            "nav-link": {
+                "font-size": "18px",
+                "text-align": "left",
+                "margin": "0px",
+                "--hover-color": "#B39DDB",
+            },
+            "nav-link-selected": {"background-color": "#8A2BE2"},
+        }
+    )
 
-        selected = option_menu(
-            menu_title="Menú Principal",
-            options=["Panel General", "Agregar Alumno", "Tomar Asistencia"],
-            icons=["house", "person-plus", "camera"],
-            menu_icon="cast",
-            default_index=0,
-            styles={
-                "container": {"padding": "5px", "background-color": "#B0B0B0"},
-                "icon": {"color": "#FFA500", "font-size": "20px"},
-                "nav-link": {
-                    "font-size": "18px",
-                    "text-align": "left",
-                    "margin": "0px",
-                    "--hover-color": "#B39DDB",
-                },
-                "nav-link-selected": {"background-color": "#8A2BE2"},
-            }
-        )
-    
-    # Lógica de enrutamiento según la selección del menú
-    if selected == "Tomar Asistencia":
-        tomar_asistencia()
-    elif selected == "Agregar Alumno":
-        agregar_alumno()
-    elif selected == "Panel General":
-        mostrar_asistencia()
+# Lógica de enrutamiento según la selección del menú
+if selected == "Tomar Asistencia":
+    tomar_asistencia()
+elif selected == "Agregar Alumno":
+    agregar_alumno()
+elif selected == "Panel General":
+    mostrar_asistencia()
